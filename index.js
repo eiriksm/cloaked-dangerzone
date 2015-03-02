739e6703af2e6780dab891dd84b3edadc2e275cb
eiriksm <eirik@morland.no>
2015-03-02 16:28:00 +0100

    Use PORT ENV

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.PORT || 8080;
 require('./lib/app').init(ip, port);
